b9823750c1953fd87b7d86f2a2835503107c413c
shawteen <aw93@disroot.org>
2024-11-19 13:45:43 +0200

    Improved build.bat

--- code/build.bat
+++ code/build.bat
@@ -4,12 +4,13 @@ set CodeDir=..\code
 set DataDir=..\data
 set OutputDir=..\build_win32
 
+If NOT EXIST %DataDir% mkdir %DataDir%
+If NOT EXIST %OutputDir% mkdir %OutputDir%
+
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 set CompilerFlags=-Od -nologo -Zi
 set LinkerFlags=-incremental:no -opt:ref user32.lib gdi32.lib
-
-If NOT EXIST %OutputDir% mkdir %OutputDir%
 
 pushd %DataDir%
 pushd %OutputDir%
